7a08523bd63e412a3471ada8056837096238bd92
Remy Oreo <remyoreo11@gmail.com>
2023-08-24 05:45:13 -0700

    made to work

--- main.js
+++ main.js
@@ -1,85 +1,84 @@
+// declarative programming
 let markup = {
-    type: 'article',
-    children: [
+  type: "article",
+  children: [
+    {
+      type: "h2",
+      children: [
         {
-            type: 'h2',
-            children: [
-                {
-                    type: 'text',
-                    value: 'counter'
-                }
-            ]
+          type: "text",
+          value: "counter",
+        },
+      ],
+    },
+    {
+      type: "h3",
+      children: [
+        {
+          type: "text",
+          value: "This works!",
+        },
+      ],
+    },
+
+    {
+      type: "p",
+      children: [
+        {
+          type: "text",
+          value: "counter",
         },
         {
-            type: 'h3',
-            children: [
+          type: "strong",
+          children: [
+            {
+              type: "em",
+              children: [
                 {
-                    type: 'text',
-                    value: 'This works!'
-                }
-            ]
+                  type: "text",
+                  value: " 1",
+                },
+              ],
+            },
+          ],
         },
+
         {
-            type: 'p',
-            children: [
-                {
-                    type: 'text',
-                    value: 'counter'
-                },
-                {
-                    type: 'em',
-                    children: [
-                        {
-                            type: 'strong',
-                            children: [
-                                {
-                                    type: 'text',
-                                    value: '1'
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    type: 'text',
-                    children: [
-                        {
-                            type: 'p',
-                            value: 'times'
-                        }
-                    ]
-                },
-                {
-                    type: 'button',
-                    children: [
-                        {
-                            type: 'text',
-                            value: 'Click me'
-                        }
-                    ]
-                }
-
-            ]
-        }
-
-    ]
-
-}
+          type: "text",
+          value: " times",
+        },
+      ],
+    },
+    {
+      type: "button",
+      children: [
+        {
+          type: "text",
+          value: "Click me",
+        },
+      ],
+    },
+  ],
+};
 
 console.log(markup);
 
 const main = document.getElementById("app");
 
-function addElements (pojoElement, parentDOMNode) {
-    let newDOMNode = pojoElement.type === "text" ? document.createTextNode(pojoElement.value)
-    : document.createElement(pojoElement.type);
-    if (pojoElement.children) {
-        pojoElement.children.forEach(child => {
-            addElements(child, newDOMNode)
-        });
-    }
-    console.log(parentDOMNode);
-    parentDOMNode.appendChild(newDOMNode);
+
+// imperative programming
+
+function addElements(pojoElement, parentDOMNode) {
+  let newDOMNode = pojoElement.type === "text"
+      ? document.createTextNode(pojoElement.value)
+      : document.createElement(pojoElement.type);
+  if (pojoElement.children) {
+    pojoElement.children.forEach((child) => {
+      addElements(child, newDOMNode);
+    });
+  }
+  console.log(parentDOMNode);
+  parentDOMNode.appendChild(newDOMNode);
 }
 
-addElements(markup, main)
+addElements(markup, main);
